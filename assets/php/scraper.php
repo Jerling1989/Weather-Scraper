76ee24a66d6d56c3b5bf0fdd3bf5b9e87f56e1e8
Jacob <jerling1989@gmail.com>
2018-02-14 02:08:52 -0500

    scraper regex

--- assets/php/scraper.php
+++ assets/php/scraper.php
@@ -1,5 +1,13 @@
 <?php
 
-	echo file_get_contents('https://www.weather-forecast.com/locations/San-Francisco/forecasts/latest');
+	$weatherSource = file_get_contents('https://www.weather-forecast.com/locations/San-Francisco/forecasts/latest');
+
+	preg_match(
+		'/<span class="phrase">(.*?)<\/span>/s',
+		$weatherSource,
+		$matches
+	);
+
+	echo $matches[1];
 
 ?>
