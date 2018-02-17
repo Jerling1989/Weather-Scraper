c525a660f8407df8af8e5e4f4c6108e56dc639ce
Jacob <jerling1989@gmail.com>
2018-02-16 22:07:37 -0500

    PHP comments

--- assets/php/scraper.php
+++ assets/php/scraper.php
@@ -1,17 +1,22 @@
 <?php
-
+	// GET VALUE FROM FROM FORM INPUT FIELD
 	$city = $_GET['city'];
 
+	// REMOVE EMPTY SPACES FROM VALUE
 	$city = str_replace(' ', '', $city);
 	
+	// GETTING HTML CONTENTS FROM WEATHER SEARCH WEBSITE AFTER ADDING USER CITY VALUE
 	$weatherSource = file_get_contents('https://www.weather-forecast.com/locations/'.$city.'/forecasts/latest');
 
+	// USING REGEX TO GATHER SPECIFIC DATA FROM WEATHER SEARCH WEBSITE (SCRAPING)
+	// ADDING FOUND DATA TO $MATCHES ARRAY 
 	preg_match(
 		'/<span class="phrase">(.*?)<\/span>/s',
 		$weatherSource,
 		$matches
 	);
 
+	// DISPLAYING DESIRED RESULT FROM SCRAPE LOCATED INSIDE $MATCHES ARRAY
 	echo $matches[1];
 
 ?>
